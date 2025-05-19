e7738c6695e292ed3face8ac7bb31c2cca277820
jysngmb <jaysongomba@gmail.com>
2025-05-19 21:54:10 +0800

    Add .next/static/chunks/src_app_cart_page_tsx_c510d031._.js 1747662827863

---
+++ .next/static/chunks/src_app_cart_page_tsx_c510d031._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_b868951f._.js",
+  "static/chunks/src_68685482._.js"
+],
+    source: "dynamic"
+});
